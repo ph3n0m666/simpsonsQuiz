fa3f47814d23d792ca80a7370a21ba65391d6b9d
ph3n0m666 <piabella@hotmail.com>
2024-01-11 18:04:57 +0000

    minor tweaks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import logoImg from "../assets/quiz-logo.png";
+import logoImg from "../assets/Homer_Simpson-removebg-preview.png";
 export default function Header() {
   return (
     <header>
-      <img src={logoImg} alt="Simpsons Quiz" />
+      <img src={logoImg} alt="Simpsons Quiz" className="square-image" />
       <h1>Simpsons Quiz</h1>
     </header>
   );
